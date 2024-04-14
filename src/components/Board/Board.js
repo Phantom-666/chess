11151ac6941c4c87ef880046c2e0d19cb39583f1
Vadim <v@gmail.com>
2024-04-15 01:03:34 +0300

    highlighting check

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -4,6 +4,8 @@ import Pieces from "./Pieces/Pieces"
 import Files from "./bits/Files"
 import Ranks from "./bits/Ranks"
 import Popup from "../Popup.js/Popup"
+import arbiter from "../../arbiter/arbiter"
+import { getKingPosition } from "../../arbiter/getMoves"
 
 const Board = () => {
   const appState = useSelector((state) => state.chess)
@@ -16,6 +18,17 @@ const Board = () => {
     .fill()
     .map((x, i) => i + 1)
 
+  const isChecked = (() => {
+    const isInCheck = arbiter.isPlayerInCheck({
+      positionAfterMove: position,
+      player: appState.turn,
+    })
+
+    if (isInCheck) return getKingPosition(position, appState.turn)
+
+    return null
+  })()
+
   const getClassName = (i, j) => {
     let c = "tile"
     c += (i + j) % 2 === 0 ? " tile-dark" : " tile-light"
@@ -25,11 +38,25 @@ const Board = () => {
       else c += " highlight"
     }
 
+    if (isChecked && isChecked[0] === i && isChecked[1] === j)
+      return (c += " checked")
+
     return c
+  }
+
+  const TurnTable = () => {
+    const turn = useSelector((state) => state.chess.turn)
+
+    return (
+      <div className={"turn-table " + `turn-table-${turn}`}>
+        <h1>Turn : {turn === "w" ? "WHITE" : "BLACK"}</h1>
+      </div>
+    )
   }
 
   return (
     <div className="board">
+      <TurnTable />
       <Ranks ranks={ranks} />
       <div className="tiles">
         {ranks.map((rank, i) =>
